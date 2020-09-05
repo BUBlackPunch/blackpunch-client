cc23602493c1036a490debef7e5b8d6d640d050c
cho chanyeong <cyjo9603@naver.com>
2020-09-05 14:45:31 +0900

    [fix] google font 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -10,6 +10,8 @@ const GlobalStyle = () => (
     styles={css`
       ${reset}
 
+      @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+
       body.sb-show-main * {
         font-family: 'Noto Sans KR', sans-serif !important;
       }
