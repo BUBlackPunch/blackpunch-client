977026d792fefb1ec4c089c1bcf3f4e44f322b67
cho chanyeong <cyjo9603@naver.com>
2020-09-05 21:51:33 +0900

    [feat] 답글 작성 버튼 컴포넌트 생성
    - 게시글 화면 우측 하단에 fixed position인 버튼 구현
    - 반응형 적용 (모바일)
    - theme에 기본 색상 추가
    - storybook 테스트 파일 작성

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,7 +1,17 @@
+const BreakPoint = {
+  HDPC: 'max-width: 1200px',
+  TABLET: 'max-width: 768px',
+  MOBILE: 'max-width: 480px',
+};
+
 const theme = {
   PRIMARY: '#084B8A',
   BUTTON_PRIMARY: '#ABCCEC',
   BUTTON_DEFAULT_FONT: '#435A6F',
+  LIGHT_BLUE: '#DBEAF7',
+  FIX_INNER: '#66788A',
+
+  BP: BreakPoint,
 };
 
 export default theme;
