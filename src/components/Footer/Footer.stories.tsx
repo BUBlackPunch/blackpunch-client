abe55c4db8200fbb6051bbf92257cb6e9bc3c609
chanyeong <cyjo9603@naver.com>
2020-09-30 16:01:13 +0900

    [feat] Footer 컴포넌트 생성
    - 레이아웃 표시할 컴포넌트 생성
    - storybook 테스트 파일 작성

---
+++ src/components/Footer/Footer.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { text } from '@storybook/addon-knobs';
+
+import Footer from './index';
+
+export default {
+  title: 'Footer',
+  component: Footer,
+};
+
+export const Default = (): JSX.Element => <Footer />;
