1d6c295e7e143028f91439ab47cd75104d5ce313
elppaaa <withbcd@gmail.com>
2022-01-30 14:55:05 +0900

    [#17] 게시글 작성 화면 (#22)

---
+++ src/components/WritePostView/WritePostView.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import WritePostView from './index';
+
+export default { title: 'WritePostView', component: WritePostView };
+
+export const Default = (): JSX.Element => {
+  return <WritePostView />;
+};
